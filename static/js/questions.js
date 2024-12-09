ae0064e373e8cd15ad19edefa3cf9fdbe035f928
boetter <37083319-boetter@users.noreply.replit.com>
2024-12-09 20:44:35 +0000

    Agent query: Can you see more questions now when playing the quiz? Are you able to progress beyond question 21?

--- static/js/questions.js
+++ static/js/questions.js
@@ -3,201 +3,218 @@ const quizQuestions = {
     {
       "question": "Hvilke lande blev medlemmer af BRICS-samarbejdet den 1. januar 2024?",
       "options": [
-          "Egypten, Etiopien, Iran, Saudi-Arabien og De Forenede Arabiske Emirater",
-          "Brasilien, Indien, Tyskland, Mexico og Tyrkiet",
-          "Canada, Australien, Argentina, Spanien og Grækenland"
+        "Egypten, Etiopien, Iran, Saudi-Arabien og De Forenede Arabiske Emirater",
+        "Brasilien, Indien, Tyskland, Mexico og Tyrkiet",
+        "Canada, Australien, Argentina, Spanien og Grækenland"
       ],
       "correct": 0
     },
     {
       "question": "Hvilken republik blev formelt opløst den 1. januar 2024, da Nagorno-Karabakh blev en del af Aserbajdsjan?",
       "options": [
-          "Republikken Tjetjenien",
-          "Republikken Artsakh",
-          "Republikken Abkhasien"
+        "Republikken Tjetjenien",
+        "Republikken Artsakh",
+        "Republikken Abkhasien"
       ],
       "correct": 1
     },
     {
       "question": "Hvilket land annoncerede den 1. januar 2024 en aftale med Somaliland om at bruge havnen i Berbera og planlagde at anerkende Somalilands uafhængighed?",
       "options": [
-          "Kenya",
-          "Etiopien",
-          "Tanzania"
+        "Kenya",
+        "Etiopien",
+        "Tanzania"
       ],
       "correct": 1
     },
     {
       "question": "Hvor kraftigt var jordskælvet, der ramte Japans vestkyst den 1. januar 2024 og resulterede i mindst 462 dødsfald?",
       "options": [
-          "Moderat styrke på 6,3",
-          "Stærkt jordskælv på 7,5",
-          "Ekstremt kraftigt på 8,0"
+        "Moderat styrke på 6,3",
+        "Stærkt jordskælv på 7,5",
+        "Ekstremt kraftigt på 8,0"
       ],
       "correct": 1
     },
     {
       "question": "Hvilken usædvanlig hændelse fandt sted under Copenhagen Fashion Week i januar 2024?",
       "options": [
-          "En kat gik på catwalken",
-          "En model faldt i en vandgrav",
-          "En drone leverede tøj til modellerne"
+        "En kat gik på catwalken",
+        "En model faldt i en vandgrav",
+        "En drone leverede tøj til modellerne"
       ],
       "correct": 0
     },
     {
       "question": "Hvilket land blev den 19. januar 2024 det femte til at gennemføre en blød landing på Månen med sin SLIM-mission?",
       "options": [
-          "Kina",
-          "Japan",
-          "Indien"
+        "Kina",
+        "Japan",
+        "Indien"
       ],
       "correct": 1
     },
     {
       "question": "Hvilken dansk dronning abdicerede den 14. januar 2024 efter 52 år på tronen?",
       "options": [
-          "Dronning Anne-Marie",
-          "Dronning Margrethe II",
-          "Dronning Elisabeth"
+        "Dronning Anne-Marie",
+        "Dronning Margrethe II",
+        "Dronning Elisabeth"
       ],
       "correct": 1
     },
     {
       "question": "Hvem blev den 14. januar 2024 udråbt som ny konge af Danmark efter Dronning Margrethes abdikation?",
       "options": [
-          "Kong Christian XI",
-          "Kong Frederik X",
-          "Kong Joachim I"
+        "Kong Christian XI",
+        "Kong Frederik X",
+        "Kong Joachim I"
       ],
       "correct": 1
     },
     {
       "question": "Hvilket land annoncerede den 18. februar 2024, at det ville donere alle sine artillerigranater til Ukraine?",
       "options": [
-          "Norge",
-          "Danmark",
-          "Sverige"
+        "Norge",
+        "Danmark",
+        "Sverige"
       ],
       "correct": 1
     },
     {
       "question": "Hvilken dansk politiker døde den 2. marts 2024 efter en hjerneblødning under et partimøde?",
       "options": [
-          "Lars Løkke Rasmussen",
-          "Søren Pape Poulsen",
-          "Kristian Thulesen Dahl"
+        "Lars Løkke Rasmussen",
+        "Søren Pape Poulsen",
+        "Kristian Thulesen Dahl"
       ],
       "correct": 1
     },
     {
       "question": "Hvilken tidligere chilenske præsident omkom i et helikopterstyrt den 6. februar 2024?",
       "options": [
-          "Michelle Bachelet",
-          "Sebastián Piñera",
-          "Ricardo Lagos"
+        "Michelle Bachelet",
+        "Sebastián Piñera",
+        "Ricardo Lagos"
       ],
       "correct": 1
     },
     {
       "question": "Hvilket land legaliserede rekreativ brug af cannabis for voksne den 23. februar 2024?",
       "options": [
-          "Tyskland",
-          "Frankrig",
-          "Spanien"
+        "Tyskland",
+        "Frankrig",
+        "Spanien"
       ],
       "correct": 0
     },
     {
       "question": "Hvilken tidligere finsk statsminister blev valgt som præsident den 11. februar 2024?",
       "options": [
-          "Sanna Marin",
-          "Alexander Stubb",
-          "Juha Sipilä"
+        "Sanna Marin",
+        "Alexander Stubb",
+        "Juha Sipilä"
       ],
       "correct": 1
     },
     {
       "question": "Hvilken russisk oppositionsleder døde i fængsel den 14. februar 2024?",
       "options": [
-          "Aleksej Navalnyj",
-          "Mikhail Khodorkovskij",
-          "Boris Nemtsov"
+        "Aleksej Navalnyj",
+        "Mikhail Khodorkovskij",
+        "Boris Nemtsov"
       ],
       "correct": 0
     },
     {
       "question": "Hvilket land blev den 22. februar 2024 det første til at lande et kommercielt fartøj på Månen?",
       "options": [
-          "USA",
-          "Kina",
-          "Japan"
+        "USA",
+        "Kina",
+        "Japan"
       ],
       "correct": 0
     },
     {
       "question": "Hvilket land blev officielt medlem af NATO den 7. marts 2024?",
       "options": [
-          "Finland",
-          "Sverige",
-          "Ukraine"
+        "Finland",
+        "Sverige",
+        "Ukraine"
       ],
       "correct": 1
     },
     {
       "question": "Hvilken lov vedtog EU den 13. marts 2024 som verdens første omfattende regulering af kunstig intelligens?",
       "options": [
-          "Data Privacy Act",
-          "Artificial Intelligence Act",
-          "Digital Services Act"
+        "Data Privacy Act",
+        "Artificial Intelligence Act",
+        "Digital Services Act"
       ],
       "correct": 1
     },
     {
       "question": "Hvilket land annoncerede den 14. marts 2024 planer om at udvide værnepligten til også at omfatte kvinder?",
       "options": [
-          "Norge",
-          "Danmark",
-          "Sverige"
+        "Norge",
+        "Danmark",
+        "Sverige"
       ],
       "correct": 1
     },
     {
       "question": "Hvilket land afholdt præsidentvalg den 15.-17. marts 2024, hvor den siddende præsident blev genvalgt for en femte periode?",
       "options": [
-          "Hviderusland",
-          "Rusland",
-          "Kasakhstan"
+        "Hviderusland",
+        "Rusland",
+        "Kasakhstan"
       ],
       "correct": 1
     },
     {
       "question": "Hvilken dansk badmintonspiller vandt guld i herresingle ved Malaysia Open den 14. januar 2024?",
       "options": [
-          "Viktor Axelsen",
-          "Anders Antonsen",
-          "Rasmus Gemke"
+        "Viktor Axelsen",
+        "Anders Antonsen",
+        "Rasmus Gemke"
       ],
       "correct": 1
     },
     {
       "question": "Hvilken forfatter udgav fortsættelsen til den prisbelønnede bestseller \"11%\" i 2024?",
       "options": [
-          "Maren Uthaug",
-          "Anne-Grethe Bjarup Riis",
-          "Thomas Rydahl"
+        "Maren Uthaug",
+        "Anne-Grethe Bjarup Riis",
+        "Thomas Rydahl"
       ],
       "correct": 0
+    },
+    {
+      "question": "Hvilket tema havde den 60. udgave af Venedig Biennalen i 2024?",
+      "options": [
+        "\"The Milk of Dreams\"",
+        "\"Foreigners Everywhere\"",
+        "\"May You Live in Interesting Times\""
+      ],
+      "correct": 1
+    },
+    {
+      "question": "Hvilken kunstudstilling i New York i efteråret 2024 inkluderede en skulptur af en due, der vakte opsigt?",
+      "options": [
+        "MoMA's efterårsudstilling",
+        "Whitney Biennalen",
+        "Guggenheims samtidsudstilling"
+      ],
+      "correct": 1
+    },
+    {
+      "question": "Hvilken international litteraturfestival fandt sted i Aarhus fra 19. til 23. juni 2024?",
+      "options": [
+        "Aarhus Literature Festival",
+        "LiteratureXchange",
+        "Aarhus Book Fair"
+      ],
+      "correct": 1
     }
   ]
 };
-
-// Assuming questions.json structure:  {"questions": [{"question": "...", "options": [...], "correct": ...}, ...]}
-fetch('questions.json')
-  .then(response => response.json())
-  .then(data => {
-    data.questions.forEach(question => {
-      quizQuestions.questions.push(question);
-    });
-  })
-  .catch(error => console.error('Error loading questions.json:', error));
